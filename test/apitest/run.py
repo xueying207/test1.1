fa29af019aeab9988e96e106d638640e766ae4f3
xueying207 <chenmiao@8760S.com>
2016-09-19 18:13:55 +0800

    Update run.py

--- test/apitest/run.py
+++ test/apitest/run.py
@@ -6,7 +6,7 @@ from sjjc  import *
 dir1='E:/\log/'
 http="http://testpj.channel.wangpiao.com/MobileTicketWebService/WebServiceV2.asmx/"
 key='Wp16dY@z^F283QVj'
-deskey="eltcwang"
+deskey="121212121"
 Des_IV = "\x12\x34\x56\x78\x90\xAB\xCD\xEF" # 自定IV向量
 
 xls=getfile(dir1)
